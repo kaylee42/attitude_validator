97c95b13905a7e60b18997e875bbe28cf24e3aee
kaylee42 <kayleeknowles42@gmail.com>
2016-01-02 19:57:23 -0500

    changes database to password instead of digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true
+  validates_presence_of :name, message: "you need a name, stupid"
   validates :password, length: { in: 6..20 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
-  validates :phone, format: { with: /\d{10}/}
+  validates :phone, format: { with: /\d{10}/, message: "Phone number must be 10 digits with no special characters."}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102213326) do
+ActiveRecord::Schema.define(version: 20160103005551) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "phone"
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
