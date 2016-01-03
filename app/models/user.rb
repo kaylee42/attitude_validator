d873104cf57b17f57f0c6e259d655dbf63519896
kaylee42 <kayleeknowles42@gmail.com>
2016-01-02 20:00:59 -0500

    finished except for error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  has_secure_password
   validates_presence_of :name, message: "you need a name, stupid"
   validates :password, length: { in: 6..20 }
+  validates :password, confirmation: true
+  validates_presence_of :password_confirmation
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
   validates :phone, format: { with: /\d{10}/, message: "Phone number must be 10 digits with no special characters."}
 end
