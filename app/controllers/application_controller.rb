3f074e2bb2e3788251c338c7323fc2218463023a
kaylee42 <kayleeknowles42@gmail.com>
2016-01-02 19:28:18 -0500

    form and validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  enable :method_override
   configure do
     set :views, "app/views"
-    enable :sessions
-    set :session_secret, "rawrdragon"
   end
 
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    user = User.new(params)
+    if user.save
+      redirect to '/'
+    else
+      @errors = user.errors.messages
+      erb :failure
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
-class User < ApplicationController::Base
-
-
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :name, presence: true
+  validates :password, length: { in: 6..20 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
+  validates :phone, format: { with: /\d{10}/}
 end
