474638b031179b4ceb318a51876610740cd363e6
nat-ser <nataliya.seryakova@gmail.com>
2016-01-03 10:36:32 -0500

    sexy formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-  validates_presence_of :name, message: "you need a name, stupid"
+  validates_presence_of :name, :password_confirmation, message: "Fill it in, duh"
   validates :password, length: { in: 6..20 }
   validates :password, confirmation: true
-  validates_presence_of :password_confirmation
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
   validates :phone, format: { with: /\d{10}/, message: "Phone number must be 10 digits with no special characters."}
 end
